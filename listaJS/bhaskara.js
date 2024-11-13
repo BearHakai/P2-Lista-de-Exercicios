87a46df15e1b386914cd7bda863927a49d027fe5
bearhakai <vinifp09@gmail.com>
2024-11-12 22:56:49 -0300

    Adicionando o exercico 16 e o 12

---
+++ listaJS/bhaskara.js
@@ -0,0 +1,43 @@
+var a = parseFloat(prompt("Informe o A da equcação: ax2 + bx + c"));
+var x1;
+var x2;
+
+if(a == 0){
+
+    alert("Está não é uma equação de segundo grau, portanto não iremos continuar.");
+    console.log("Está não é uma equação de segundo grau, portanto não iremos continuar.");
+}
+
+else if(a != 0){
+    
+    var b = parseFloat(prompt("Informe o B da equcação: ax2 + bx + c"));
+    var c = parseFloat(prompt("Informe o C da equcação: ax2 + bx + c"));
+    var delta = (b * b) - 4 * a * c;
+
+    if(delta < 0){
+        alert("A equação não possui raízes reais, portanto não iremos continuar." + delta);
+        console.log("A equação não possui raízes reais, portanto não iremos continuar.");
+    }
+    
+    else if(delta == 0){
+        alert("A equação possui 1 raiz real.");
+        console.log("A equação possui 1 raiz real.");
+
+        x1 = (- b + Math.sqrt(delta)) / (2 * a);
+        x2 = (- b - Math.sqrt(delta)) / (2 * a);
+        
+        alert("A respota seria aproximadamente: " + x1);
+        console.log("A respota seria aproximadamente: " + x1);
+    }
+    
+    else if(delta > 0){
+        alert("A equação possui 2 raizes reais.");
+        console.log("A equação possui 2 raizes reais.");
+
+        x1 = (- b + Math.sqrt(delta)) / (2 * a);
+        x2 = (- b - Math.sqrt(delta)) / (2 * a);
+
+        alert("A respota seria aproximadamente: " + x1 + " ou " + x2);
+        console.log("A respota seria aproximadamente: " + x1 + " ou " + x2);
+    }
+}
--- listaJS/folhaPagamento.js
+++ listaJS/folhaPagamento.js
@@ -1,3 +1,4 @@
+//Informe os dados para calcular as informacoes do calculo do salario
 var horas = parseInt(prompt("Informe a quantidade de horas que trabalha no dia: "));
 var dias = parseInt(prompt("Informe quantos dias você trabalha no mês: "));
 var salarioDia = parseFloat(prompt("Informe quanto você ganha por hora trabalhada: "));
@@ -5,5 +6,45 @@ var salario = (horas * salarioDia) * dias;
 var imposto;
 var sindicado = salario * 0.03;
 var fgts = salario * 0.08;
+var salarioLiquido;
 
-//if(salario > )
+//Vai comecar as fazer as decisoes de qual sera o desconto do seu salario para mostrar o salario bruto
+if(salario <= 2259.20){
+    imposto = 0;
+    salarioLiquido = salario - imposto - sindicado;
+    alert("O Salario: R$ " + salario + "\nO Imposto: R$ " + imposto + "\nO Sindicado: R$ " + sindicado + "\nO FGTS: R$ " + fgts + "O salario final é: R$" + salarioLiquido);
+    console.log("O Salario: R$ " + salario + "\nO Imposto: R$ " + imposto + "\nO Sindicado: R$ " + sindicado + "\nO FGTS: R$ " + fgts + "O salario final é: R$" + salarioLiquido);
+}
+
+else if(salario >= 2259.21 && salario <= 2826.65){
+    imposto = salario * 0.075;
+    salarioLiquido = salario - imposto - sindicado;
+    alert("O Salario: R$ " + salario + "\nO Imposto: R$ " + imposto + "\nO Sindicado: R$ " + sindicado + "\nO FGTS: R$ " + fgts + "O salario final é: R$" + salarioLiquido);
+    console.log("O Salario: R$ " + salario + "\nO Imposto: R$ " + imposto + "\nO Sindicado: R$ " + sindicado + "\nO FGTS: R$ " + fgts + "O salario final é: R$" + salarioLiquido);
+}
+
+else if(salario >= 2826.66 && salario <= 3751.05 ){
+    imposto = salario * 0.15;
+    salarioLiquido = salario - imposto - sindicado;
+    alert("O Salario: R$ " + salario + "\nO Imposto: R$ " + imposto + "\nO Sindicado: R$ " + sindicado + "\nO FGTS: R$ " + fgts + "O salario final é: R$" + salarioLiquido);
+    console.log("O Salario: R$ " + salario + "\nO Imposto: R$ " + imposto + "\nO Sindicado: R$ " + sindicado + "\nO FGTS: R$ " + fgts + "O salario final é: R$" + salarioLiquido);
+}
+
+else if(salario >= 3751.06 && salario <= 4664.68 ){
+    imposto = salario * 0.225;
+    salarioLiquido = salario - imposto - sindicado;
+    alert("O Salario: R$ " + salario + "\nO Imposto: R$ " + imposto + "\nO Sindicado: R$ " + sindicado + "\nO FGTS: R$ " + fgts + "O salario final é: R$" + salarioLiquido);
+    console.log("O Salario: R$ " + salario + "\nO Imposto: R$ " + imposto + "\nO Sindicado: R$ " + sindicado + "\nO FGTS: R$ " + fgts + "O salario final é: R$" + salarioLiquido);
+}
+
+else if(salario > 4664.68){
+    imposto = salario * 0.275;
+    salarioLiquido = salario - imposto - sindicado;
+    alert("O Salario: R$ " + salario + "\nO Imposto: R$ " + imposto + "\nO Sindicado: R$ " + sindicado + "\nO FGTS: R$ " + fgts + "O salario final é: R$" + salarioLiquido);
+    console.log("O Salario: R$ " + salario + "\nO Imposto: R$ " + imposto + "\nO Sindicado: R$ " + sindicado + "\nO FGTS: R$ " + fgts + "O salario final é: R$" + salarioLiquido);
+}
+
+else{
+    alert("Não é um valor válido!");
+    console.log("Não é um valor válido!");
+}
